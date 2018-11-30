b110e6f1660d983e6afea40a351291c7baa49cf7
abhishek <abhishekgupta753@gmail.com>
2018-11-30 00:49:50 -0800

    sql

---
+++ Lotto649 Export Loader Format/Generated-20171107160451.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Tuesday-November-07-2017   
+--------------------------------------------------------
+@C:\Users\C0448413\Desktop\databaselab7\Lotto649 Export Loader Format\LOTTO649.sql
+--@C:\Users\C0448413\Desktop\databaselab7\Lotto649 Export Loader Format\LOTTO649_DATA_TABLE.ldr
+--@C:\Users\C0448413\Desktop\databaselab7\Lotto649 Export Loader Format\LOTTO649_DATA_TABLE.ctl
+@C:\Users\C0448413\Desktop\databaselab7\Lotto649 Export Loader Format\SYS_C007032.sql
+@C:\Users\C0448413\Desktop\databaselab7\Lotto649 Export Loader Format\LOTTO649_CONSTRAINT.sql
